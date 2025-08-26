0c03f4475792d0eb8304f3a27c19ab6f42df210a
doukn0wdaway <anisimov.sasha370@gmail.com>
2025-08-26 20:53:51 +0300

    tmux added to scroll lock key

--- modules/tmux/default.nix
+++ modules/tmux/default.nix
@@ -3,5 +3,15 @@
   pkgs,
   ...
 }: {
-  home.packages = with pkgs; [tmux];
+  programs.tmux = {
+    enable = true;
+    baseIndex = 1;
+    keyMode = "vi";
+    terminal = "tmux-256color";
+    prefix = "F12";
+    # extraConfig = ''
+    #   unbind C-b
+    #    set -g prefix F12
+    # '';
+  };
 }
