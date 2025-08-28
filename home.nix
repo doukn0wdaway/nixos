3f7b34c1380145e8fb241f3c643e3b618279a22e
doukn0wdaway <anisimov.sasha370@gmail.com>
2025-08-29 00:51:38 +0300

    something

--- home.nix
+++ home.nix
@@ -66,10 +66,6 @@
     };
   };
 
-  # programs.lutris = {
-  #   enable = true;
-  # };
-
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
 
   # plain files is through 'home.file'.
