1ef667441bbc94f343bb79af0bd80394a10fcf5e
doukn0wdaway <anisimov.sasha370@gmail.com>
2025-08-30 21:33:42 +0300

    fixed bug with escape
    ESC IN TMUX OR GNU SCREEN IS DELAYED

--- modules/tmux/default.nix
+++ modules/tmux/default.nix
@@ -8,6 +8,7 @@
     keyMode = "vi";
     terminal = "tmux-256color";
     prefix = "F12";
+    escapeTime = 5;
     # extraConfig = ''
     #   unbind C-b
     #    set -g prefix F12
--- modules/vim/config/lua/config/keymaps.lua
+++ modules/vim/config/lua/config/keymaps.lua
@@ -5,3 +5,9 @@ vim.keymap.set("n", "gR", "<cmd> Trouble lsp_references <CR>")
 
 vim.keymap.set("n", "<tab>", "<cmd> bn <CR>")
 vim.keymap.set("n", "<S-tab>", "<cmd> bp <CR>")
+for _, mode in ipairs({ "n", "i", "v", "x", "s", "o", "c", "t" }) do
+  vim.keymap.set(mode, "<M-j>", "<Nop>", { silent = true })
+  vim.keymap.set(mode, "<M-k>", "<Nop>", { silent = true })
+end
+
+vim.keymap.del("n", "<Esc>") -- по дефу оно вызывает очистку хайлайта
